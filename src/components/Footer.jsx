f8c15c5d44cf36a7470499c1ba7059ccd69d19cd
hanzala <hanzala.khan4269252@gmail.com>
2024-09-04 14:05:52 -0700

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Section from "./Section";
+import { socials } from "../constants";
+
+const Footer = () => {
+  return (
+    <Section crosses className="!px-0 !py-10">
+      <div className="container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col">
+        <p className="caption text-n-4 lg:block">
+          <a
+            href="https://hanzala-portfolio-f2456.web.app"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-n-4 hover:underline"
+          >
+            Hanzala's Portfolio
+          </a>{" "}
+          © {new Date().getFullYear()}. All rights reserved.
+        </p>
+
+        <ul className="flex gap-5 flex-wrap">
+          {socials.map((item) => (
+            <a
+              key={item.id}
+              href={item.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center justify-center w-10 h-10 bg-n-7 rounded-full transition-colors hover:bg-n-6"
+            >
+              <img src={item.iconUrl} width={16} height={16} alt={item.title} />
+            </a>
+          ))}
+        </ul>
+      </div>
+    </Section>
+  );
+};
+
+export default Footer;
